7f076f12f1bfa5af6976f8873dc856ea30d583e9
dima <AlertDSG@gmail.com>
2022-04-04 16:52:01 +0300

    Completed task 1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import css from './Message.module.css'
 
-function Message() {
+type TypeMessage = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+export const Message = (props: TypeMessage) => {
     return (
-        <div>
-
+        <div className={css.message}>
+            <img src={props.avatar} alt="avatar"/>
+            <div className={css.content}>
+                <div className={css.text}>
+                    <h3>{props.name}</h3>
+                    <h2>{props.message}</h2>
+                </div>
+                <div className={css.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
 
-export default Message
+
