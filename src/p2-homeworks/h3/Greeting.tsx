990e620a6b9c92df18167ac4e2f0d24c23736422
dima <AlertDSG@gmail.com>
2022-04-20 17:58:22 +0300

    Refactor task 3-4 css and components 'input and button'

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,5 +1,7 @@
 import React, {ChangeEvent} from 'react'
 import s from './Greeting.module.css'
+import SuperInputText from "../h4/common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 type GreetingPropsType = {
     name: string // need to fix any
@@ -13,13 +15,15 @@ type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = error === 'Attention - name is required' ? s.error : ''// need to fix with (?:)
+    const inputClass = error ? s.error : ''// need to fix with (?:)
 
     return (
         <div className={s.someClass}>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <button onClick={addUser}>add</button>
-            <div>{error}</div>
+            <SuperInputText error={error} value={name} className={inputClass} onChange={setNameCallback}/>
+            {/*<input value={name} onChange={setNameCallback} className={s.inputClass}/>*/}
+           <div> <SuperButton onClick={addUser} children={"add"} className={s.someClass}/></div>
+            {/*<button onClick={addUser}>add</button>*/}
+            {/*<div className={s.color}>{error}</div>*/}
             <span>{totalUsers}</span>
         </div>
     )
