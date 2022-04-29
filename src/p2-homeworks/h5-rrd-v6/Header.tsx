a32b7254e9ccd725e62fcd8c07fd2395b8de8fee
dima <AlertDSG@gmail.com>
2022-04-29 19:26:23 +0300

    Completed task 5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {PATH} from "./Pages";
+import {NavLink} from "react-router-dom";
+import s from './Hw5.module.css'
+
 
 function Header() {
+
+    let [click, setClick] = useState<boolean>(false);
+    const linksPage = click ? s.menu : s.none
+    const onClickHandler = () => {
+        setClick(!click)
+    }
     return (
         <div>
-            // add NavLinks
-
+            <div onClick={onClickHandler} className={s.arrow} title={'Click me!'}>
+                &#9759;
+            </div>
+            <ul className={linksPage}>
+                <li>
+                    <NavLink to={PATH.PRE_JUNIOR} className={navData => navData.isActive ? s.active : s.item}>
+                        Pre-junior
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={PATH.JUNIOR} className={navData => navData.isActive ? s.active : s.item}>
+                        Junior
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={PATH.JUNIOR_PLUS} className={navData => navData.isActive ? s.active : s.item}>
+                        Junior+
+                    </NavLink>
+                </li>
+            </ul>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Junior = () => {
+    return (
+        <div>I am Junior</div>
+    )
+}
+
+export default Junior;
