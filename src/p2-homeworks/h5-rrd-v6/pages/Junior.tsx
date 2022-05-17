4272d894072e005a38194cc0d152b5277137621e
dima <AlertDSG@gmail.com>
2022-05-18 01:35:48 +0300

    Completed task 8

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import HW7 from "../../h7/HW7";
+import HW8 from "../../h8/HW8";
+import s from "../../h8/HW8.module.css"
 
 const Junior = () => {
     return (
-        <HW7/>
+        <div className={s.wrapper }>
+            <HW7/>
+            <HW8/>
+        </div>
     )
 }
 
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,27 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UsersType} from "../HW8";
+
+type SortType = {
+    type: 'sort'
+    payload: 'up' | 'down'
+}
+
+type CheckType = {
+    type: 'check'
+    payload: number
+}
+
+
+export const homeWorkReducer = (state: UsersType, action: SortType | CheckType): UsersType => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            return action.payload === 'up'
+                ? [...state].sort((a, b) => a.name > b.name ? 1 : -1)
+                : [...state].sort((a, b) => a.name < b.name ? 1 : -1)
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(s => s.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
